2c25272e4b732ac7f114cd3cfb47d75606ce07d1
embuh75 <coolbanditz75@gmail.com>
2026-03-23 10:03:10 +0700

    nambah home page & komponen terkait

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import ProductCard from "../components/ProductCard";
+import { products } from "../lib/data/prosucts";
+
+export default function Home() {
+  return (
+    <div className="page">
+      <div className="home-hero">
+        <h1 className="home-title">Welcome to Daimaou Shop</h1>
+        <p className="home-subtitle">Discover amazing product at low price!</p>
+      </div>
+      <div className="container">
+        <h2 className="page-title">Discover Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
